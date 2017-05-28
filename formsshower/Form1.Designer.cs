af8252cb5a8ed9f91ddd734784fed904cd3f825f
OlegBrest <pop457045@mail.ru>
2017-05-28 08:54:58 +0300

    Image filter

--- formsshower/Form1.Designer.cs
+++ formsshower/Form1.Designer.cs
@@ -28,12 +28,214 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.PictureViewer = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chanse_txtbx = new System.Windows.Forms.MaskedTextBox();
+            this.bttn_Grey = new System.Windows.Forms.Button();
+            this.bttn_Filter = new System.Windows.Forms.Button();
+            this.bttn_Noise = new System.Windows.Forms.Button();
+            this.bttn_Load = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.porog_txtbx = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chk_AutoFilter = new System.Windows.Forms.CheckBox();
+            this.txtbx_StepAuto = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureViewer)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.PictureViewer, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(871, 469);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // PictureViewer
+            // 
+            this.PictureViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PictureViewer.Location = new System.Drawing.Point(3, 3);
+            this.PictureViewer.Name = "PictureViewer";
+            this.PictureViewer.Size = new System.Drawing.Size(865, 418);
+            this.PictureViewer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureViewer.TabIndex = 0;
+            this.PictureViewer.TabStop = false;
+            this.PictureViewer.WaitOnLoad = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.chk_AutoFilter);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtbx_StepAuto);
+            this.panel1.Controls.Add(this.porog_txtbx);
+            this.panel1.Controls.Add(this.chanse_txtbx);
+            this.panel1.Controls.Add(this.bttn_Grey);
+            this.panel1.Controls.Add(this.bttn_Filter);
+            this.panel1.Controls.Add(this.bttn_Noise);
+            this.panel1.Controls.Add(this.bttn_Load);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 427);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(865, 39);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "% заполнения";
+            // 
+            // chanse_txtbx
+            // 
+            this.chanse_txtbx.Location = new System.Drawing.Point(184, 16);
+            this.chanse_txtbx.Mask = "00";
+            this.chanse_txtbx.Name = "chanse_txtbx";
+            this.chanse_txtbx.Size = new System.Drawing.Size(24, 20);
+            this.chanse_txtbx.TabIndex = 4;
+            this.chanse_txtbx.Text = "10";
+            // 
+            // bttn_Grey
+            // 
+            this.bttn_Grey.Location = new System.Drawing.Point(296, 16);
+            this.bttn_Grey.Name = "bttn_Grey";
+            this.bttn_Grey.Size = new System.Drawing.Size(75, 23);
+            this.bttn_Grey.TabIndex = 3;
+            this.bttn_Grey.Text = "Grey";
+            this.bttn_Grey.UseVisualStyleBackColor = true;
+            this.bttn_Grey.Click += new System.EventHandler(this.bttn_Grey_Click);
+            // 
+            // bttn_Filter
+            // 
+            this.bttn_Filter.Location = new System.Drawing.Point(496, 16);
+            this.bttn_Filter.Name = "bttn_Filter";
+            this.bttn_Filter.Size = new System.Drawing.Size(75, 23);
+            this.bttn_Filter.TabIndex = 2;
+            this.bttn_Filter.Text = "Filter";
+            this.bttn_Filter.UseVisualStyleBackColor = true;
+            this.bttn_Filter.Click += new System.EventHandler(this.bttn_Filter_Click);
+            // 
+            // bttn_Noise
+            // 
+            this.bttn_Noise.Location = new System.Drawing.Point(88, 16);
+            this.bttn_Noise.Name = "bttn_Noise";
+            this.bttn_Noise.Size = new System.Drawing.Size(75, 23);
+            this.bttn_Noise.TabIndex = 1;
+            this.bttn_Noise.Text = "Noise";
+            this.bttn_Noise.UseVisualStyleBackColor = true;
+            this.bttn_Noise.Click += new System.EventHandler(this.bttn_Noise_Click);
+            // 
+            // bttn_Load
+            // 
+            this.bttn_Load.Location = new System.Drawing.Point(0, 16);
+            this.bttn_Load.Name = "bttn_Load";
+            this.bttn_Load.Size = new System.Drawing.Size(75, 23);
+            this.bttn_Load.TabIndex = 0;
+            this.bttn_Load.Text = "Load";
+            this.bttn_Load.UseVisualStyleBackColor = true;
+            this.bttn_Load.Click += new System.EventHandler(this.bttn_Load_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "\"Images |*.bmp;*.png;*.jpg;*.jpeg|All files|*.*\"";
+            // 
+            // porog_txtbx
+            // 
+            this.porog_txtbx.Location = new System.Drawing.Point(576, 16);
+            this.porog_txtbx.Mask = "900";
+            this.porog_txtbx.Name = "porog_txtbx";
+            this.porog_txtbx.Size = new System.Drawing.Size(24, 20);
+            this.porog_txtbx.TabIndex = 4;
+            this.porog_txtbx.Text = "100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(544, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Порог фильтра";
+            // 
+            // chk_AutoFilter
+            // 
+            this.chk_AutoFilter.AutoSize = true;
+            this.chk_AutoFilter.Location = new System.Drawing.Point(616, 16);
+            this.chk_AutoFilter.Name = "chk_AutoFilter";
+            this.chk_AutoFilter.Size = new System.Drawing.Size(70, 17);
+            this.chk_AutoFilter.TabIndex = 6;
+            this.chk_AutoFilter.Text = "AutoFilter";
+            this.chk_AutoFilter.UseVisualStyleBackColor = true;
+            // 
+            // txtbx_StepAuto
+            // 
+            this.txtbx_StepAuto.Location = new System.Drawing.Point(688, 16);
+            this.txtbx_StepAuto.Mask = "00";
+            this.txtbx_StepAuto.Name = "txtbx_StepAuto";
+            this.txtbx_StepAuto.Size = new System.Drawing.Size(24, 20);
+            this.txtbx_StepAuto.TabIndex = 4;
+            this.txtbx_StepAuto.Text = "5";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(672, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "AutoFilter Step";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(871, 469);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Баловство всё это";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureViewer)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox PictureViewer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button bttn_Grey;
+        private System.Windows.Forms.Button bttn_Filter;
+        private System.Windows.Forms.Button bttn_Noise;
+        private System.Windows.Forms.Button bttn_Load;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.MaskedTextBox chanse_txtbx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox porog_txtbx;
+        private System.Windows.Forms.CheckBox chk_AutoFilter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox txtbx_StepAuto;
     }
 }
 
--- formsshower/Form1.cs
+++ formsshower/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,275 @@ namespace formsshower
 {
     public partial class Form1 : Form
     {
+        String FileName = "";
+        Bitmap OriginPicture;
+        Random rnd1 = new Random();
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void bttn_Load_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                FileName = openFileDialog.FileName;
+            }
+            using (FileStream fs = new FileStream(FileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+                OriginPicture = new Bitmap(fs);
+            PictureViewer.Image = OriginPicture;
+        }
+
+        private void bttn_Noise_Click(object sender, EventArgs e)
+        {
+            long total = OriginPicture.Size.Width * OriginPicture.Size.Height;
+            total = (total * Convert.ToInt32(chanse_txtbx.Text)) / 100;
+            Bitmap NoisePicture = new Bitmap(PictureViewer.Image);
+            /*            for (int x = 0; x < OriginPicture.Size.Width; x++)
+                        {
+                            for (int y = 0; y < OriginPicture.Size.Height; y++)
+                            {
+                                int chans = rnd1.Next(100);
+                                if (chans < Convert.ToInt32(chanse_txtbx.Text))
+                                {
+                                    NoisePicture.SetPixel(x, y, Color.FromArgb(255, rnd1.Next(255), rnd1.Next(255), rnd1.Next(255)));
+                                }
+                            }
+                        }
+                        */
+            for (long i = 0; i < total; i++) NoisePicture.SetPixel(rnd1.Next(OriginPicture.Size.Width), rnd1.Next(OriginPicture.Size.Height), Color.FromArgb(255, rnd1.Next(255), rnd1.Next(255), rnd1.Next(255)));
+            PictureViewer.Image = NoisePicture;
+        }
+
+        private unsafe void bttn_Filter_Click(object sender, EventArgs e)
+        {
+            bool AutoFilter = chk_AutoFilter.Checked;
+            int step = Convert.ToInt32(txtbx_StepAuto.Text);
+            int porog = Convert.ToInt32(porog_txtbx.Text);
+            if (AutoFilter)
+            {
+                Bitmap bmp = new Bitmap(PictureViewer.Image);
+                int width = bmp.Width,
+                height = bmp.Height;
+                byte[,,] res = new byte[3, height, width];
+                BitmapData bd = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly,
+                    PixelFormat.Format24bppRgb);
+                try
+                {
+                    byte* curpos;
+                    fixed (byte* _res = res)
+                    {
+                        byte* _r = _res, _g = _res + width * height, _b = _res + 2 * width * height;
+                        for (int h = 0; h < height; h++)
+                        {
+                            curpos = ((byte*)bd.Scan0) + h * bd.Stride;
+                            for (int w = 0; w < width; w++)
+                            {
+                                *_b = *(curpos++); ++_b;
+                                *_g = *(curpos++); ++_g;
+                                *_r = *(curpos++); ++_r;
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    bmp.UnlockBits(bd);
+                }
+                byte[,,] work_res = new byte[3, height, width];
+                work_res = res;
+                int Prec = 3; // по несколько раз на высокой точности
+                do
+                {
+                    do
+                    {
+                        for (int y = 1; y < width - 1; y++)
+                        {
+                            for (int x = 1; x < height - 1; x++)
+                            {
+                                int R = 0, G = 0, B = 0;
+                                bool R11 = false, G11 = false, B11 = false;
+
+                                int R_por = res[0, x, y];
+                                int G_por = res[1, x, y];
+                                int B_por = res[2, x, y];
+
+                                for (int j = 0; j < 3; j++)
+                                {
+                                    for (int k = 0; k < 3; k++)
+                                    {
+
+                                        if ((j != 1) || (k != 1))
+                                        {
+
+                                            if (Math.Abs(res[0, x - 1 + j, y - 1 + k] - R_por) < porog) R11 = true;
+                                            if (Math.Abs(res[1, x - 1 + j, y - 1 + k] - G_por) < porog) G11 = true;
+                                            if (Math.Abs(res[2, x - 1 + j, y - 1 + k] - B_por) < porog) B11 = true;
+
+                                            R += res[0, x - 1 + j, y - 1 + k];
+                                            G += res[1, x - 1 + j, y - 1 + k];
+                                            B += res[2, x - 1 + j, y - 1 + k];
+                                        }
+                                    }
+                                }
+
+                                R /= 8;
+                                G /= 8;
+                                B /= 8;
+
+                                if ((R11) && (G11) && (B11))
+                                {
+                                    R = R_por;
+                                    G = G_por;
+                                    B = B_por;
+                                }
+                                work_res[0, x, y] = (byte)R;
+                                work_res[1, x, y] = (byte)G;
+                                work_res[2, x, y] = (byte)B;
+                                bmp.SetPixel(y, x, Color.FromArgb(255, R, G, B));
+                            }
+                        }
+                        Prec++; 
+                    } while (Prec < 5);
+
+                    if (AutoFilter)
+                    {
+                        porog -= step;
+                        porog_txtbx.Text = porog.ToString();
+                        porog_txtbx.Refresh();
+                        PictureViewer.Image = bmp;
+                        PictureViewer.Refresh();
+                        res = work_res;
+                    }
+
+                    if (porog < (step)) AutoFilter = false;
+
+                    if (porog < 25) Prec = 0;
+                } while (AutoFilter);
+
+            }
+            else
+            {
+                do
+                {
+                    Bitmap FilterImage = new Bitmap(PictureViewer.Image);
+                    Bitmap WorkImage = new Bitmap(PictureViewer.Image);
+                    PictureViewer.Image = FilterImage;
+                    int[,] R_matrix = new int[3, 3];
+                    int[,] G_matrix = new int[3, 3];
+                    int[,] B_matrix = new int[3, 3];
+                    for (int x = 1; x < WorkImage.Size.Width - 1; x++)
+                    {
+                        for (int y = 1; y < WorkImage.Size.Height - 1; y++)
+                        {
+                            int R = 0, G = 0, B = 0;
+                            bool R11 = false, G11 = false, B11 = false;
+                            for (int j = 0; j < 3; j++)
+                            {
+                                for (int k = 0; k < 3; k++)
+                                {
+                                    Color clr = WorkImage.GetPixel(x - 1 + j, y - 1 + k);
+                                    R_matrix[j, k] = clr.R;
+                                    G_matrix[j, k] = clr.G;
+                                    B_matrix[j, k] = clr.B;
+                                }
+                            }
+                            for (int j = 0; j < 3; j++)
+                            {
+                                for (int k = 0; k < 3; k++)
+                                {
+                                    if ((j != 1) || (k != 1))
+                                    {
+                                        if (Math.Abs(R_matrix[j, k] - R_matrix[1, 1]) < porog) R11 = true;
+                                        if (Math.Abs(G_matrix[j, k] - G_matrix[1, 1]) < porog) G11 = true;
+                                        if (Math.Abs(B_matrix[j, k] - B_matrix[1, 1]) < porog) B11 = true;
+                                    }
+                                }
+                            }
+
+                            if ((R11) && (G11) && (B11))
+                            {
+                                R = R_matrix[1, 1];
+                                G = G_matrix[1, 1];
+                                B = B_matrix[1, 1];
+                            }
+                            else
+                            {
+                                for (int j = 0; j < 3; j++)
+                                {
+                                    for (int k = 0; k < 3; k++)
+                                    {
+                                        if ((j != 1) || (k != 1))
+                                        {
+                                            R += R_matrix[j, k];
+                                            G += G_matrix[j, k];
+                                            B += B_matrix[j, k];
+                                        }
+                                    }
+                                }
+                                R /= 8;
+                                G /= 8;
+                                B /= 8;
+                            }
+                            FilterImage.SetPixel(x, y, Color.FromArgb(255, R, G, B));
+                        }
+
+                        if ((x > 3) && (x < (FilterImage.Size.Width - 3)) && ((x / (AutoFilter ? 30 : 5)) == ((double)x / (AutoFilter ? 30 : 5))))
+                        {
+                            int xx = x - 2;
+                            for (int yy = 0; yy < FilterImage.Size.Height; yy++)
+                            {
+                                Color tmp_pixel = FilterImage.GetPixel(xx, yy);
+                                FilterImage.SetPixel(xx, yy, Color.FromArgb(255, 255 - tmp_pixel.R, 255 - tmp_pixel.G, 255 - tmp_pixel.B));
+                            }
+                            PictureViewer.Refresh();
+                            for (int yy = 0; yy < FilterImage.Size.Height; yy++)
+                            {
+                                Color tmp_pixel = FilterImage.GetPixel(xx, yy);
+                                FilterImage.SetPixel(xx, yy, Color.FromArgb(255, 255 - tmp_pixel.R, 255 - tmp_pixel.G, 255 - tmp_pixel.B));
+                            }
+                        }
+                    }
+                    PictureViewer.Refresh();
+                    // PictureViewer.Image = FilterImage;
+                    if (AutoFilter)
+                    {
+                        porog -= step;
+                        porog_txtbx.Text = porog.ToString();
+                        porog_txtbx.Refresh();
+                    }
+                    if (porog < (step * 2)) AutoFilter = false;
+                } while (AutoFilter);
+            }
+        }
+        private void bttn_Grey_Click(object sender, EventArgs e)
+        {
+            Bitmap GreyImage = new Bitmap(PictureViewer.Image);
+            Bitmap WorkImage = new Bitmap(PictureViewer.Image);
+
+            for (int x = 0; x < WorkImage.Size.Width; x++)
+            {
+                for (int y = 0; y < WorkImage.Size.Height; y++)
+                {
+                    int C = 0;
+                    Color clr = WorkImage.GetPixel(x, y);
+                    C += clr.R;
+                    C += clr.G;
+                    C += clr.B;
+                    C /= 3;
+                    GreyImage.SetPixel(x, y, Color.FromArgb(255, C, C, C));
+                }
+            }
+            PictureViewer.Image = GreyImage;
         }
     }
 }
